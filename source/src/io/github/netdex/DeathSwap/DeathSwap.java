9f14d541bbfd4ad749196844e281bf596bada356
Netdex <netdex593@gmail.com>
2014-07-25 21:51:08 -0400

    Version 3.5a
    - Removed abilities
    - Added random chance of acquiring items

--- source/src/io/github/netdex/DeathSwap/DeathSwap.java
+++ source/src/io/github/netdex/DeathSwap/DeathSwap.java
@@ -5,16 +5,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.FireworkEffect;
-import org.bukkit.FireworkEffect.Type;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,7 +19,6 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -37,31 +32,31 @@ public class DeathSwap extends JavaPlugin implements Listener {
 	public Plugin plugin;
 	public static FileConfiguration config;
 	public static int maxPlayers;
-	public static HashMap<String, Long> smashCool = new HashMap<String, Long>();
 	
+
 	public void onEnable() {
 		// Load and create configuration
 		try{
 			config = getConfig();
 			File DeathSwap = new File("plugins" + File.separator + "DeathSwap" + File.separator + "config.yml");
 			DeathSwap.mkdir();
-			
+
 			PlayerInterface.setConfig();
 			saveConfig();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		
+
 		this.plugin = this;
 		getServer().getPluginManager().registerEvents(this, this); // Register listeners
 		PlayerInterface.loadWorld(); // Load world
 		this.getCommand("ds").setExecutor(new CommandManager(this));
 		this.getCommand("dsa").setExecutor(new CommandManager(this));
-		
+
 		defaultWorld = PlayerInterface.parseLocation(config.getString("defaultWorld"));
 		maxPlayers = DeathSwap.config.getInt("maxPlayers");
 	}
-	
+
 	public void onDisable() {
 		getLogger().info("Removing all players...");
 		if(gameRunning){
@@ -73,7 +68,7 @@ public class DeathSwap extends JavaPlugin implements Listener {
 			}
 		}
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onDeath(PlayerDeathEvent event){ // Check dying players
@@ -86,7 +81,7 @@ public class DeathSwap extends JavaPlugin implements Listener {
 			PlayerInterface.checkWinner();
 		}
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	/**
@@ -94,21 +89,21 @@ public class DeathSwap extends JavaPlugin implements Listener {
 	 * then tell everyone and themselves that they have left, then teleport the player back to the spawn.
 	 * After that, check for a winner.
 	 */
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        String name = event.getPlayer().getName();
-        if(playerQueue.contains(name)){
-        	playerQueue.remove(name);
-        	if(gameRunning){
-            	PlayerInterface.playerBroadcast(name + " has left DeathSwap.");
-            	Bukkit.getPlayer(name).teleport(defaultWorld);
-            	PlayerInterface.checkWinner();
-        	}
-        	else{
-        		PlayerInterface.playerBroadcast(name + " has been kicked out of the DeathSwap queue.");
-        	}
-        }
-    }
-	
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		String name = event.getPlayer().getName();
+		if(playerQueue.contains(name)){
+			playerQueue.remove(name);
+			if(gameRunning){
+				PlayerInterface.playerBroadcast(name + " has left DeathSwap.");
+				Bukkit.getPlayer(name).teleport(defaultWorld);
+				PlayerInterface.checkWinner();
+			}
+			else{
+				PlayerInterface.playerBroadcast(name + " has been kicked out of the DeathSwap queue.");
+			}
+		}
+	}
+
 	@EventHandler
 	public void onVehicleRide(org.bukkit.event.vehicle.VehicleEnterEvent event){
 		String name = ((Player) event.getEntered()).getName();
@@ -117,7 +112,7 @@ public class DeathSwap extends JavaPlugin implements Listener {
 			PlayerInterface.sendMessage((Player) event.getEntered(), "You cannot ride anything during DeathSwap.");
 		}
 	}
-	
+
 	@EventHandler
 	public void onPortalTeleport(org.bukkit.event.player.PlayerPortalEvent event){
 		String name = event.getPlayer().getName();
@@ -126,45 +121,5 @@ public class DeathSwap extends JavaPlugin implements Listener {
 			PlayerInterface.sendMessage(event.getPlayer(), "You may not portal during DeathSwap.");
 		}
 	}
-	
-	@EventHandler
-	public void onPlayerInteract(org.bukkit.event.player.PlayerInteractEvent event){
-		if(event.getAction() == Action.RIGHT_CLICK_BLOCK){
-			Player player = event.getPlayer();
-			if(playerQueue.contains(player.getName()) && gameRunning){
-				if(smashCool.get(player.getName()) == null){
-					smashCool.put(player.getName(), (long) 30001);
-				}
-				long cooldown = smashCool.get(player.getName());
-				long elapsed = System.currentTimeMillis() - cooldown;
-				PlayerInventory inv = player.getInventory();
-				ItemStack held = inv.getItemInHand();
-				if(held.getType() == Material.GOLD_AXE){ // Check held item
-					if(elapsed > 30000){ // Check cooldown
-						PlayerInterface.playerBroadcast(player.getName() + " used entity smash!");
-						
-						Firework fw = (Firework) player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
-						FireworkMeta fwm = fw.getFireworkMeta();
-						FireworkEffect effect = FireworkEffect.builder().withColor(Color.RED).withColor(Color.ORANGE).withColor(Color.YELLOW).with(Type.BALL_LARGE).build();
-						fwm.addEffects(effect);
-						fwm.setPower(0); 
-						fw.setFireworkMeta(fwm);
-						
-						for(Entity e : player.getNearbyEntities(10, 10, 10)){ // Set fire to all entities within 10 block radius
-							e.setFireTicks(300);
-						}
-						smashCool.put(player.getName(), System.currentTimeMillis());
-					}
-					else{ // Cooldown not complete
-						int remaining = (int) ((30000 - elapsed)/1000);
-						PlayerInterface.sendMessage(player,"You must wait " + remaining + " seconds before using this ability again.");
-					}
-				}
-				else{
-					
-				}
-			}
-		}
-		
-	}
+
 }
--- source/src/io/github/netdex/DeathSwap/PlayerSwapper.java
+++ source/src/io/github/netdex/DeathSwap/PlayerSwapper.java
@@ -1,13 +1,21 @@
 package io.github.netdex.DeathSwap;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerSwapper extends Thread {
-	public static volatile boolean isRunning = true;
+	private static volatile boolean isRunning = true;
+	private static Random r = new Random();
 	
 	public void run(){
 		// Get values from config
@@ -15,7 +23,6 @@ public class PlayerSwapper extends Thread {
 		int minSwap = DeathSwap.config.getInt("minSwapTime");
 		int maxSwap = DeathSwap.config.getInt("maxSwapTime");
 		
-		Random r = new Random();
 		try {Thread.sleep(graceTime * 1000);} catch (InterruptedException e1) {}
 		while(isRunning){
 			PlayerInterface.playerBroadcast("Swap!");
@@ -37,15 +44,57 @@ public class PlayerSwapper extends Thread {
 	
 	@SuppressWarnings("deprecation")
 	public static void swap(){
-		Location firstPlayer = Bukkit.getServer().getPlayer(DeathSwap.playerQueue.get(0)).getLocation();
-		for(int i = 0; i < DeathSwap.playerQueue.size(); i++){
-			Player p = Bukkit.getServer().getPlayer(DeathSwap.playerQueue.get(i));
-			if(i == DeathSwap.playerQueue.size() - 1){
+		ArrayList<String> randomNames = new ArrayList<String>();
+		for(String name : DeathSwap.playerQueue){ // Copy the list
+			randomNames.add(name);
+		}
+		Collections.shuffle(randomNames); // Shuffle
+		Location firstPlayer = Bukkit.getServer().getPlayer(randomNames.get(0)).getLocation();
+		for(int i = 0; i < randomNames.size(); i++){
+			Player p = Bukkit.getServer().getPlayer(randomNames.get(i));
+			if(i == randomNames.size() - 1){
 				p.teleport(firstPlayer);
 			}
 			else{
-				p.teleport(Bukkit.getServer().getPlayer(DeathSwap.playerQueue.get(i+1)).getLocation());
+				p.teleport(Bukkit.getServer().getPlayer(randomNames.get(i+1)).getLocation());
 			}
+			p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200 ,10));
+			giveGift(p);
 		}
+	}
+	
+	public static void giveGift(Player player){
+		int giveChance = r.nextInt(5);
+		if(giveChance == 0){
+			PlayerInventory inv = player.getInventory();
+			int rand = r.nextInt(10);
+			if(rand >= 0 && rand < 5){
+				PlayerInterface.sendMessage(player, "You have been gifted some redstone!");
+				ItemStack redstone = new ItemStack(Material.REDSTONE, 3);
+				inv.addItem(redstone);
+			}
+			else if(rand >= 5 && rand < 7){
+				PlayerInterface.sendMessage(player, "You have been gifted some obsidian!");
+				ItemStack obsidian = new ItemStack(Material.OBSIDIAN, 3);
+				inv.addItem(obsidian);
+			}
+			else if(rand == 7){
+				PlayerInterface.sendMessage(player, "You have been gifted some TNT!");
+				ItemStack tnt = new ItemStack(Material.TNT, 1);
+				inv.addItem(tnt);
+			}
+			else if(rand == 8){
+				PlayerInterface.sendMessage(player, "You have been gifted a blaze rod!");
+				ItemStack blazerod = new ItemStack(Material.BLAZE_ROD, 1);
+				inv.addItem(blazerod);
+			}
+			else if(rand == 9){
+				PlayerInterface.sendMessage(player, "You have been gifted a piston!");
+				ItemStack piston = new ItemStack(Material.PISTON_STICKY_BASE, 1);
+				inv.addItem(piston);
+			}
+			
+		}
+		
 	}
 }
